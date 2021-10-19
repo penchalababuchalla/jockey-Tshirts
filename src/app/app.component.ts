61513fae326045703db89fee6f660619db7781fa
Penchala Babu <penchalababuchalla@gmail.com>
2021-10-19 08:02:46 +0530

    filter functionality changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component,HostListener } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Router, ActivatedRoute, ParamMap, Params } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +14,7 @@ export class AppComponent {
   listOfProducts:any;
   changetxt : boolean;
   sortBy= "";
+  avlFit:boolean;
   avlSize:boolean;
   avlStyle:boolean;
   avlColor:boolean;
@@ -25,6 +26,8 @@ export class AppComponent {
   filtersList: any;
   logo:boolean = true;
   size:any;
+
+  totalRecords = 0;
   
   sizeS=false;
   sizeM=false;
@@ -57,164 +60,186 @@ export class AppComponent {
   pageNumber=1;
   avblProducts: any;
 
+  fitFiltersList:any = {}
+  sizeFiltersList:any = {}
+  styleFiltersList:any = {}
+  colorFiltersList:any = {}
+  fabricFiltersList:any = {}
+  sleeveFiltersList:any = {}
+
   constructor(private http: HttpClient,private router: Router, private route: ActivatedRoute){
     
     this.router = router;
-    this.route = route;
 
     var url = 'https://api.jockey.in/api/productsbystyles?cat1=men&cat2=apparel-tops&cat3=t-shirts&cat4=&pno=1&ps=12&by=&ctkn=&dir=&f='
     this.http.get<any>(url).subscribe(data => {
       this.filtersList=data.Data.Facets
       this.avblProducts = data.Data.StyleDetails
       this.listOfProducts = this.avblProducts
+      // this.totalRecords = data.Data.RowCount
     });
     this.changetxt = false;
+    this.avlFit = true;
     this.avlSize = true;
     this.avlStyle = true;
     this.avlColor = true;
     this.avlFabric = true;
     this.avlSleeve = true;
-  }    
-  sortingOrder(){
+  }   
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      let appliedFitFiltersStr = params['fit'];
+      if(appliedFitFiltersStr){
+        let appliedFitFilters = appliedFitFiltersStr.split(':');
+        for(let i=0;i<appliedFitFilters.length;i++){
+          this.fitFiltersList[appliedFitFilters[i]] = true;
+        }
+      }
+      let appliedSizeFiltersStr = params['size'];
+      if(appliedSizeFiltersStr){
+        let appliedSizeFilters = appliedSizeFiltersStr.split(':');
+        for(let i=0; i < appliedSizeFilters.length; i++) {
+          this.sizeFiltersList[appliedSizeFilters[i]] = true;
+        }
+      }
+      let appliedStyleFiltersStr = params['style'];
+      if(appliedStyleFiltersStr){
+        let appliedStyleFilters = appliedStyleFiltersStr.split(':');
+        for(let i=0; i < appliedStyleFilters.length; i++) {
+          this.styleFiltersList[appliedStyleFilters[i]] = true;
+        }
+      }
+      let appliedColorFiltersStr = params['color'];
+      if(appliedColorFiltersStr){
+        let appliedColorFilters = appliedColorFiltersStr.split(':');
+        for(let i=0; i < appliedColorFilters.length; i++) {
+          this.colorFiltersList[appliedColorFilters[i]] = true;
+        }
+      }
+      let appliedFabricFiltersStr = params['fabric'];
+      if(appliedFabricFiltersStr){
+        let appliedFabricFilters = appliedFabricFiltersStr.split(':');
+        for(let i=0;i<appliedFabricFilters.length;i++){
+          this.fabricFiltersList[appliedFabricFilters[i]]=true;
+        }
+      }
+      let appliedSleeveFiltersStr = params['sleeve'];
+      if(appliedSleeveFiltersStr){
+        let appliedSleeveFilters = appliedSleeveFiltersStr.split(':');
+        for(let i=0; i < appliedSleeveFilters.length; i++) {
+          this.sleeveFiltersList[appliedSleeveFilters[i]] = true;
+        }
+      }
+      this.sortingOrder(false)
+    });
+  } 
+  sortingOrder(navigate=true){
+    // debugger
     var url="cat1=men&cat2=apparel-tops&cat3=t-shirts&cat4=&ps=12&ctkn=&f=";
-    url=url + `&pno=`+this.pageNumber
+
+    var availableFitFilterUrl = Object.keys(this.fitFiltersList);
+    availableFitFilterUrl = availableFitFilterUrl.filter(url => this.fitFiltersList[url]==true);
+    var fitFiltersString = availableFitFilterUrl.join(":");
+    if(fitFiltersString!=""){
+      url=url + `fittype=`+fitFiltersString + `|`
+    }
+    var availableSizeFilterUrl = Object.keys(this.sizeFiltersList);
+    availableSizeFilterUrl = availableSizeFilterUrl.filter(url => this.sizeFiltersList[url]==true);
+    var sizeFiltersString = availableSizeFilterUrl.join(":");
+    if( sizeFiltersString !=""){
+      url = url + `size=`+sizeFiltersString + `|`
+    }
+    var availableStyleFilterUrl = Object.keys(this.styleFiltersList);
+    availableStyleFilterUrl = availableStyleFilterUrl.filter(url => this.styleFiltersList[url]==true);
+    var styleFiltersString = availableStyleFilterUrl.join(":");
+    if(styleFiltersString !=""){
+      url = url + `style=`+styleFiltersString + `|`
+    }
+    var availableColorFilterUrl = Object.keys(this.colorFiltersList);
+    availableColorFilterUrl = availableColorFilterUrl.filter(url => this.colorFiltersList[url]==true);
+    var colorFiltersString = availableColorFilterUrl.join(":");
+    if(colorFiltersString!=""){
+      url = url+`color=`+colorFiltersString +`|`
+    }
+    var availableFabricFilterUrl = Object.keys(this.fabricFiltersList);
+    availableFabricFilterUrl = availableFabricFilterUrl.filter(url => this.fabricFiltersList[url]==true);
+    var fabricFiltersString = availableFabricFilterUrl.join(":");
+    if(fabricFiltersString!=""){
+      url = url+`fabric=`+fabricFiltersString +`|`
+    }
+    var availableSizeFilterUrl = Object.keys(this.sleeveFiltersList);
+    availableSizeFilterUrl = availableSizeFilterUrl.filter(url => this.sleeveFiltersList[url]==true);
+    var sleeveFiltersString = availableSizeFilterUrl.join(":");
+    if(sleeveFiltersString!=""){
+      url = url+`sleeve=`+sleeveFiltersString+`|`
+    }
+
     if(this.sortBy=="popular"){
-      debugger
       url = url+`&by=popular` 
     } else if(this.sortBy=="price: High to Low"){
-      debugger
-      url = url+`&by=price&dir=desc&`
+      url = url+`&by=price&dir=desc`
     } else if(this.sortBy=="price: Low to High"){
-      debugger
-      url = url+`&by=price&dir=asc&`
+      url = url+`&by=price&dir=asc`
     } else if(this.sortBy=="bestseller"){
-      debugger
       url = url+`&by=bestseller`
     }   
-    // let sizeFilters: string[] = [];
-    // if(this.sizeS==true){
-    //   sizeFilters.push("s")
-    //   this.router.navigateByUrl('/?size=' + sizeFilters)
-    // } else {
-    //   this.router.navigateByUrl('/?size=' + sizeFilters)
-    // }
-    // if(this.sizeM==true){
-    //   sizeFilters.push("m")
-    // }   
-    // if(this.sizeL==true){
-    //   sizeFilters.push("l")
-    // }
-    // if(this.sizeXL==true){
-    //   sizeFilters.push("xl")
-    // }
-    // if(this.sizeXXL==true){
-    //   sizeFilters.push("xxl")
-    // }
-    // var sizeFiltersStrings = sizeFilters.join(":")
-    
-    
-    // let sleeveFilters:string[] = []
-    // if(this.fullSleeve==true){
-    //   sleeveFilters.push("full-sleeves")
-    //   this.showHalfSleeve=true;
-    //   this.showRaglanSleeve=true;
-    // }
-    // if(this.halfSleeve==true){
-    //   sleeveFilters.push("half-sleeves")
-    //   this.showRaglanSleeve=true;
-    //   this.showFullSleeve=true;
-    // }
-    // if(this.raglanSleeve==true){
-    //   sleeveFilters.push("raglan-sleeves")
-    //   this.showFullSleeve=true;
-    //   this.showHalfSleeve=true;
-    // }
+ 
+    if(navigate) {
+      const queryParams: Params = {};
 
-    // let styleFilters:string[]= []
-    // if(this.styl2714==true){
-    //   styleFilters.push("2714%20-%20sport%20t-shirt")
-    // }
-    // if(this.styl2715==true){
-    //   styleFilters.push("2715%20-%20crew%20neck%20t-shirt")
-    // }
-    // if(this.styl2717==true){
-    //   styleFilters.push("2717%20-%20sport%20t-shirt")
-    // }
-    // if(this.styl2726==true){
-    //   styleFilters.push("2726%20-%20v-neck%20t-shirt")
-    // }
-    // if(this.stylAM50==true){
-    //   styleFilters.push("am50%20-%20v-neck%20t-shirt")
-    // }
-    // if(this.stylAM51==true){
-    //   styleFilters.push("am51%20-%20t-shirt")
-    // }
-    // var styleFiltersSrings = styleFilters.join(":")
-    
-    // let colorFilters:string[] = []
-    // if(this.clrRed==true){
-    //   colorFilters.push("red")
-    // }
-    // if(this.clrBlack==true){
-    //   colorFilters.push("black")
-    // }
-    // if(this.clrBlue==true){
-    //   colorFilters.push("blue")
-    // }
-    // if(this.clrGreen==true){
-    //   colorFilters.push("green")
-    // }
-    // if(this.clrOrange==true){
-    //   colorFilters.push("orange")
-    // }
-    // if(this.clrWhite==true){
-    //   colorFilters.push("white")
-    // }
-    // if(this.clrYellow==true){
-    //   colorFilters.push("yellow")
-    // }
-    // var colorFiltersString = colorFilters.join(":")
-    
-    // let fabricFilters:string[] = []
-    // if(this.Cotton==true){
-    //   fabricFilters.push("cotton")
-    //   this.plstr=true;
-    //   this.ctnPlstr=true;
-    // }
-    // if(this.CtnPolstr==true){
-    //   fabricFilters.push("cotton-and-polyester")
-    //   this.ctn=true;
-    //   this.plstr=true;
-    // } else {
-    //   this.plstr=false;
-    //   this.ctn=false;
-    // }
-    // if(this.Polyester==true){
-    //   fabricFilters.push("polyester")
-    //   this.ctn=true;
-    //   this.ctnPlstr=true;
-    // } else {
-    //   this.ctn=false;
-    //   this.ctnPlstr=false;
-    // }
-     
-    // url = url + `size=`+sizeFiltersStrings + `|`
-    // url = url +`style=`+styleFiltersSrings + `|`
-    // url = url + `fabric=`+ fabricFilters +  `|`
-    // url = url + `sleeve=`+ sleeveFilters + `|`
-    // url = url + `color=`+ colorFiltersString + `|`
-    
-    if(this.pageNumber==2){
+      if(fitFiltersString!=""){
+        queryParams['fit'] = fitFiltersString;
+      }
+      if(sizeFiltersString != ""){
+        queryParams['size'] = sizeFiltersString;
+      }
+      if(colorFiltersString != ""){
+        queryParams['color'] = colorFiltersString;
+      }
+      if(styleFiltersString != ""){
+        queryParams['style'] =styleFiltersString;
+      }
+      if(fabricFiltersString != ""){
+        queryParams['fabric'] = fabricFiltersString;
+      }
+      if(sleeveFiltersString != ""){
+        queryParams['sleeve'] = sleeveFiltersString;
+      }
+      this.router.navigate(
+        [], 
+        {
+          relativeTo: this.route,
+          queryParams: queryParams, 
+      });
+    }
+    url=url + `&pno=`+this.pageNumber
+    if(this.pageNumber > 1){
+      debugger
       this.http.get<any>('https://api.jockey.in/api/productsbystyles?'+url).subscribe(data => {
+        this.logo=true;
         this.listOfProducts = this.listOfProducts.concat(data.Data.StyleDetails)
+        // this.totalRecords = data.Data.RowCount
       }); 
     } else {
+      debugger
       this.http.get<any>('https://api.jockey.in/api/productsbystyles?'+url).subscribe(data => {
-        debugger
-        this.listOfProducts = data.Data.StyleDetails
+        if(data.Data.StyleDetails == null){
+          this.listOfProducts = []
+        } else {
+          this.listOfProducts = data.Data.StyleDetails;
+          // this.totalRecords = data.Data.RowCount
+        }
       }); 
     }
   } 
+  showFit(){
+    debugger
+    if(this.avlFit==true){
+      this.avlFit = false;
+    }else {
+      this.avlFit = true;
+    }
+  }
   showSize(){
     // this.avlSize = false;
     if(this.avlSize==true){
@@ -256,25 +281,26 @@ export class AppComponent {
       if (this.listOfProducts[i].ProductId == productId) {
         for(var j = 0; j < this.listOfProducts[i].ProductDetails.length; j++){
           if (this.listOfProducts[i].ProductDetails[j].ColorName == colorName) {
+            this.listOfProducts[i].changetxt = true;
             this.listOfProducts[i].activeImageUrl =  this.listOfProducts[i].ProductDetails[j].ListImagePath + this.listOfProducts[i].ProductDetails[j].Images.split('|')[0];
             this.listOfProducts[i].activeTitle = this.listOfProducts[i].ProductDetails[j].ProductTitle;
+            
           }
         }
       }
     }
   }
-  sizeFilter(i:any,j:any){
-    this.filtersList[i].size[j]
-  }
   @HostListener("window:scroll", ["$event"])
   onWindowScroll(){
-    this.logo=false;
-    if(window.scrollY + document.documentElement.clientHeight > document.documentElement.offsetHeight - 500){
-      this.pageNumber = this.pageNumber +1
-     if(this.pageNumber == 2 ){
-       this.logo=true;
-      this.sortingOrder();
-     }     
-    }    
-  }
+     if(window.scrollY + document.documentElement.clientHeight > document.documentElement.offsetHeight - 500){
+      debugger
+      // if(this.totalRecords >  this.listOfProducts.length){
+        this.pageNumber = this.pageNumber +1
+        
+        if(this.pageNumber==2){
+          this.logo=false;
+          this.sortingOrder();
+        }     
+     }    
+   }
 }
--- src/app/token-interceptor.service.ts
+++ src/app/token-interceptor.service.ts
@@ -10,7 +10,7 @@ export class AuthInterceptor implements HttpInterceptor {
     constructor(private authService: AuthService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        const authToken = ' eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzaWQiOiI5YTE4YjNlOC1lYjkzLTQ4MzQtYjI3OC1jMDczNzEzZTNiMTIiLCJhaWQiOiJDQUQ1MDU5MC1BMTg5LTQxNEYtQTJGNS0xMDMzOUQxRjlGOTciLCJkaWQiOiI0NDQ5NkIwMy0zNjQ3LTQ5MEMtQjJGMC0yRTE1OUIyN0QyNzgiLCJuYmYiOjE2MzQxODU3NzUsImV4cCI6MTYzNDI3MjE3NSwiaWF0IjoxNjM0MTg1Nzc1fQ.iyhPlhsX3xtpD0MEIiVvs0goig4ubMy0Tqt55s55RKA';
+        const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzaWQiOiIxZDViODRjOC1iODUwLTRhZmQtOWU2NC0zNzNiOTcwMmRmZWIiLCJhaWQiOiJDQUQ1MDU5MC1BMTg5LTQxNEYtQTJGNS0xMDMzOUQxRjlGOTciLCJkaWQiOiI0NDQ5NkIwMy0zNjQ3LTQ5MEMtQjJGMC0yRTE1OUIyN0QyNzgiLCJuYmYiOjE2MzQ1MjMzMzcsImV4cCI6MTYzNDYwOTczNywiaWF0IjoxNjM0NTIzMzM3fQ.PYSOILOmdZnubpAUfGCRXrEO6dNyAg2x4At69euLEOs';
         req = req.clone({
             setHeaders: {
                 Authorization: "Bearer " + authToken
